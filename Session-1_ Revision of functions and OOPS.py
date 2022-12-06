9f81ab51b62ff54016a30d15d5c66c19e529949a
Shree7676 <shreekantnandiyawar@gmail.com>
2022-12-06 20:55:09 +0530

    Home work done

--- Session-1_ Revision of functions and OOPS.py
+++ Session-1_ Revision of functions and OOPS.py
@@ -61,6 +61,8 @@ convertedAmount = convertCurrency(96, "rupees", "pounds")
 convertedAmount = str(convertedAmount[0]) + " " + convertedAmount[1]
 print(convertedAmount)
 
+# Home Work
+
 """
 Anil wants to calculate the tax to be paid on his earnings. 
 He doesn’t remember the amount charged on the earnings. 
@@ -70,6 +72,14 @@ information and started creating the program and now he is happy to use the calc
 Create the same kind of program so that you can use it in the future.
 """
 
+def Tax_calculator(amount,tax_rate):
+    return amount*(tax_rate/100)
+
+amt=int(input("What is your salary"))
+tax=int(input("what is the tax rate "))
+
+a=Tax_calculator(amt,tax)
+print(f"Your tax amount is {a}")
 
 """
 Chris loves open-world games. He wants to create an open-world game 
@@ -79,3 +89,54 @@ create a class vehicle and then different child classes for different types of v
 Help Chris in doing the same.
 """
 
+class vehicle:
+    def __init__(self,brand,colour):
+        self.brand=brand
+        self.colour=colour
+        self.tyre=4
+        self.headlight="OFF"
+        self.engine="OFF"
+
+    def start(self):
+        self.engine = "ON"
+        print("Engine Started")
+    def stop(self):
+        self.engine = "OFF"
+        print("Engine Stoped")
+    def headlight_ON(self):
+        self.headlight = "ON"
+        print("Engine Started")
+    def headlight_OFF(self):
+        self.headlight = "OFF"
+        print("Engine Stoped")
+
+class trucks(vehicle):
+    def __init__(self,brand,colour):
+        super(trucks, self).__init__(brand,colour)
+        self.tyre=8
+
+class cars(vehicle):
+    pass
+
+class police_cars(cars):
+    def __init__(self,brand,colour):
+        super(police_cars, self).__init__(brand,colour)
+        self.siren="OFF"
+        self.Beep=("RED","BLUE")
+    def siren_ON(self):
+        self.siren = "ON"
+        print("'siren on' on high volume ")
+    def siren_OFF(self):
+        self.siren = "OFF"
+        print("siren off")
+    def Beep_ON(self):
+        self.Beep = "ON"
+        print("red blue light beep display...")
+    def Beep_OFF(self):
+        self.Beep = "OFF"
+        print("red blue light stop blinking...")
+
+class ambulances(police_cars):
+    pass
+
+#No check as per need in main pgm
---
+++ Session-2_ Revision of tkinter.py
@@ -0,0 +1,16 @@
+#Class work
+
+"""
+Ayman and his other friends planned to play cricket.
+They tossed a coin to decide between batting and balling.
+Ayman got an idea to create a virtual coin in python GUI programming.
+He created a GUI program with a button toss and each time he clicks the button
+he gets a random side of the coin. Create the same program and toss the coin.
+"""
+
+"""
+Shaurya plays tic tac toe ( X O ) on the last page of his notebook. 
+One day he thought to create a board for this game using the python Tkinter module. 
+He was able to create the board successfully and played the game with his friends as well. 
+Write a program to create a python GUI ( X O ) board and play with your friends.
+"""
